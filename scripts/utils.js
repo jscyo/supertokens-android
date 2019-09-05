9b6e94b70d81eb5cd706b16f8ddacc85ce9fa9f7
Rishabh Poddar <rishabh.poddar@gmail.com>
2019-09-05 11:40:12 +0000

    [skip ci]
    Deploy website version based on b23e919345318616a882284774d41a505c4d1bd0

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,3 +1,9 @@
+let latestVersion = "0.0.0"; /*THIS COMMENT IS USED TO REPLACE THE LATEST VERSION*/
+
 function getGithubUrl() {
     return "https://github.com/supertokens/supertokens-android";
+}
+
+function getLatestVersion() {
+    return latestVersion;
 }
